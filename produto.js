3cfa83444413feee8778ec525a5dc5df6bdc4555
KevinGabriel18 <kgsenaxavier25@gmail.com>
2024-10-08 11:13:42 -0300

    petshop responsivo passando produto para o carrinho

--- produto.js
+++
@@ -1,8 +0,0 @@
-function selectProduct(productName) {
-    let selectedProducts = JSON.parse(localStorage.getItem('selectedProducts')) || [];
-    if (!selectedProducts.includes(productName)) {
-        selectedProducts.push(productName);
-        localStorage.setItem('selectedProducts', JSON.stringify(selectedProducts));
-    }
-    window.location.href = 'carrinho.html';
-}
